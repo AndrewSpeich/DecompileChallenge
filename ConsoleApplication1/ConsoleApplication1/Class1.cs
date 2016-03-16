2a50c885c5a3b1b41215b855137eb77bde0b61e7
Andrew Speich <andrewspeich@outlook.com>
2016-03-16 11:38:45 -0500

    Edited the variables of chr, chr1, chr2

---
+++ ConsoleApplication1/ConsoleApplication1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public Program()
+        {
+        }
+
+        private static void Main(string[] args)
+        {
+            (new RomanConversions()).MainBuild();
+        }
+    }
+}
